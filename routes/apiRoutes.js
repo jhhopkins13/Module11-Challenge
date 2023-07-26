95d59a7acb358178acfecc10522fe6c8e10dc4d2
John Henry Hopkins <jhhopkins13@gmail.com>
2023-07-26 13:00:06 -0500

    Delete still doesn't work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 function readNotes() {
   const data = fs.readFileSync('./db/db.json', 'utf8');
   return JSON.parse(data);
 }
 
-function saveNote(note) {
-  const notes = readNotes();
-  notes.push(note);
+function saveNotes(notes) {
   fs.writeFileSync('./db/db.json', JSON.stringify(notes));
 }
 
@@ -19,9 +18,22 @@ router.get('/notes', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
-  const newNote = req.body;
-  saveNote(newNote);
+  const newNote = {
+    id: uuidv4(),
+    ...req.body,
+  };
+  const notes = readNotes();
+  notes.push(newNote);
+  saveNotes(notes);
   res.json(newNote);
+});
+
+router.delete('/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  const notes = readNotes();
+  const updatedNotes = notes.filter((note) => note.id !== noteId);
+  saveNotes(updatedNotes);
+  res.json({ message: 'Note deleted successfully.' });
 });
 
 module.exports = router;
